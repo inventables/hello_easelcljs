e92e87ca4c609cc2f62d761a362642144861a5cd
David Altenburg <david@inventables.com>
2016-05-27 14:53:08 -0500

    Generate method is now done in CLJS

--- src/hello_easelcljs/core.cljs
+++ src/hello_easelcljs/core.cljs
@@ -1,7 +1,40 @@
 (ns hello-easelcljs.core)
 
+(def quantity-property {:id "Circles" :type "range" :min 1 :max 100 :step 1 :value 50})
+
+(def size-property {:id "Size" :type "range" :min 1 :max 100 :step 1 :value 50})
+
+(defn- material-depth [js-args]
+  (get-in (js->clj js-args) ["material" "dimensions" "z"]))
+
+(defn- random-depth [material-thickness]
+  (* material-thickness
+     (/ (.ceil js/Math (* 8 (rand))) 16)))
+
+(defn- canned-rectangle [material-thickness]
+  {
+   :shape {
+           :type "rectangle"
+           :flipping {}
+           :center {:x 5 :y 5}
+           :width 10
+           :height 10
+           :rotation 2
+           }
+   :cut {
+         :depth (random-depth material-thickness)
+         :type "outline"
+         :outlineStyle "on-path"
+         }
+   })
+
+(defn- generate-circles-within-canned-rectangle [material-thickness]
+  (let [rectangle (canned-rectangle material-thickness)]
+    [rectangle]))
+
 (defn load-properties []
-  (clj->js [{:type "range", :id "Width", :value 4, :min 1, :max 10, :step 1}
-            {:type "range", :id "Height", :value 4, :min 1, :max 10, :step 1}
-            {:type "range", :id "Radius", :value 0, :min 0, :max 10, :step 1}
-            {:type "range", :id "Something else", :value 0, :min 0, :max 10, :step 1}]))
+  (clj->js [quantity-property size-property]))
+
+(defn executor [js-args js-success js-failure]
+  (js-success (clj->js (generate-circles-within-canned-rectangle
+                        (material-depth js-args)))))
