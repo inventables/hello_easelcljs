f8ce88d163fc06b3f78aef7cda1a5245e0a95a39
David Altenburg <david@inventables.com>
2016-05-27 11:19:56 -0500

    Building properties in the clojurescript file

--- src/hello_easelcljs/core.cljs
+++ src/hello_easelcljs/core.cljs
@@ -1,3 +1,7 @@
 (ns hello-easelcljs.core)
 
-(.log js/console "Loaded some CLJS")
+(defn load-properties []
+  (clj->js [{:type "range", :id "Width", :value 4, :min 1, :max 10, :step 1}
+            {:type "range", :id "Height", :value 4, :min 1, :max 10, :step 1}
+            {:type "range", :id "Radius", :value 0, :min 0, :max 10, :step 1}
+            {:type "range", :id "Something else", :value 0, :min 0, :max 10, :step 1}]))
